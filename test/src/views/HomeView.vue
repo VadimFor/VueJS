4009cef888cc24e6b38eff33f87c295096ce8fc1
Vadim <99600085+VadimFor@users.noreply.github.com>
2024-01-27 14:54:58 +0100

    Update HomeView.vue

--- test/src/views/HomeView.vue
+++ test/src/views/HomeView.vue
@@ -5,5 +5,17 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+
+    <div class="products-list">
+      <div class="product"   >
+        <div class="item1">item 1 </div>
+        <div class="item2">item 2 </div>
+        <div class="item3">item 3 </div>
+        <div class="item4">item 4 </div>  
+        <div class="item5">item 5 </div>
+        <div class="item6">item 6 </div>  
+      </div>
+ </div>
+
   </main>
 </template>
